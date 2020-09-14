a0e3f36561650b38183f7d582e6e6dc56979b2a0
Cezary Pokropek <cezary.pokropek@gmail.com>
2020-09-14 18:43:56 +0200

    created simple app with Recycler View functionallity

--- PortfolioApp/app/src/main/java/com/cezarypokropek/portfolioapp/MainActivity.java
+++ PortfolioApp/app/src/main/java/com/cezarypokropek/portfolioapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cezarypokropek.portfolioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
+        Project[] projects = {
+                new Project("Restaurant App", "Restaurant App using Android Studio and Java", R.drawable.hungry_developer),
+                new Project("Task Timer App", "Task Timer App using Android Studio and Java with custom datebase", R.drawable.calculator),
+                new Project("Flickr Clone App", "Flickr-like App using Android Studio and Java", R.drawable.quote),
+                new Project("Trivia App", "Trivia Quiz Game App using Android Studio and Java", R.drawable.getting_started),
+                new Project("Earth Quake App", "EarthQuake App using Android Studio and Java with google maps", R.drawable.tape)
+        };
+
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(adapter);
+
+
     }
+
 }
